3b5ee560f8f9f64ddece45f9cddb7ab5c650c2f8
Danylo Hermanovych <elluyema@gmail.com>
2022-10-18 00:54:59 -0700

    lab-1: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:promise/recommended",
+    "plugin:n/recommended",
+    "plugin:sonarjs/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript"
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": 1,
+    "@typescript-eslint/no-unused-vars": 2,
+  },
+};
